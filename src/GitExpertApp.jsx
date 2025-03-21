c5d5b5fa4029c2b278e058c2d82fb4fb00ba7c07
Eder <eder.arodriguez1999@gmail.com>
2025-03-21 11:42:14 -0600

    Componente AddCategory

--- src/GitExpertApp.jsx
+++ src/GitExpertApp.jsx
@@ -1,12 +1,34 @@
-
+import { useState } from "react"
+import AddCategory from "./components/AddCategory";
 
 export default function GitExpertApp() {
-  return (
-    <>
-        <h1>GitExpertApp</h1>
-    </>
+    const [categories, setCategories] = useState(['One Punch', 'Eder'])
 
-  )
+    const onAddCategory = () => {
+        const newCategory = 'Nueva Categoria';
+        if(categories.includes(newCategory)) return
+
+        setCategories([...categories, newCategory]);
+    }
 
 
+    return (
+        <>
+            {/** Titulo */}
+            <h1>GitExpertApp</h1>
+
+            {/** Input */}
+            <AddCategory />
+        
+            {/** Listado de Gif */}
+            <button onClick={onAddCategory}>Agregar</button>
+
+            <ol>
+                { categories.map( category => {
+                    return <li key={category}>{category}</li>
+                }) }
+            </ol>
+        </>
+
+    )
 }
