ba843a3740a3ed1f2b1cd61ee2d398db998a14d9
Eder <eder.arodriguez1999@gmail.com>
2025-03-25 11:04:20 -0600

    Mostrando mensaje de carga - spinner

--- src/components/GifGrid.jsx
+++ src/components/GifGrid.jsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from "react";
 import GifItem from "./GifItem";
 import { useFetchGifs } from "../hooks/useFetchGifs";
 
 export default function GifGrid({ category }) {
     const { images, isLoading } = useFetchGifs ( category );
-    console.log({ isLoading});
 
     return (
         <>
             <h3>{ category }</h3>
+            {
+               isLoading && ( <h2>Cargando...</h2> ) 
+            }
+
             <div className="card-grid">
                 {
                     images.map( ( image ) => (
